ff9ae933ad8ae1aed75a1ccf8292bedf49db7560
temoki <santemoki@gmail.com>
2017-12-07 12:20:35 +0600

    Added view

--- blog/views.py
+++ blog/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render
 from django.utils import timezone
 from .models import Post
-Post.objects.get(pk=pk)
+
 def post_detail(request, pk):
     post = get_object_or_404(Post, pk=pk)
     return render(request, 'blog/post_detail.html', {'post': post})
